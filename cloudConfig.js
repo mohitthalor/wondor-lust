fed034e1f3f6d3f26b23d194a27744afa4faf543
mohit thalor <120308813+mohitthalor@users.noreply.github.com>
2024-05-03 01:01:31 +0530

    cloudConfig.js

--- cloudConfig.js
+++ cloudConfig.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    CLOUD_NAME:dbhhn8i6i,
-    API_KEY: process.env.CLOUD_API_KEY,
-    API_SECRET: process.env.CLOUD_API_SECRET
+    CLOUD_NAME:'dbhhn8i6i',
+    API_KEY: '426882378774778',
+    API_SECRET: 'k7F87LIKamUXk2ZFSvOypA7KJqM'
 });
 
 const storage = new CloudinaryStorage({
